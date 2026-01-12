f67ddc40c371aec97268d55a68b9db8653a9fa0a
abirdoesfrontend <abirkhan.official.tsx@gmail.com>
2026-01-12 22:06:33 +0600

    fix: linting and build issues

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,44 +1,50 @@
-import React from 'react'
-import {fetcher} from "@/lib/coingecko.actions";
+import React from "react";
+import { fetcher } from "@/lib/coingecko.actions";
 import Image from "next/image";
-import {formatCurrency} from "@/lib/utils";
-import {CoinOverviewFallback} from "@/components/home/fallback";
+import { formatCurrency } from "@/lib/utils";
+import { CoinOverviewFallback } from "@/components/home/fallback";
 import CandleSticksChart from "@/components/CandleSticksChart";
 
-const CoinOverview = async() => {
-    try {
-        const [coin, coinOHLCData] = await Promise.all([
-            await fetcher<CoinDetailsData>('/coins/bitcoin', {
-                dex_pair_format: 'symbol'
-            }),
+const CoinOverview = async () => {
+  let data;
+  try {
+    data = await Promise.all([
+      fetcher<CoinDetailsData>("/coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
 
-            await fetcher<OHLCData[]>('/coins/bitcoin/ohlc', {
-                vs_currency: 'usd',
-                days: 1,
-                precision: 'full',
-            })
-        ])
+      fetcher<OHLCData[]>("/coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: 1,
+        precision: "full",
+      }),
+    ]);
+  } catch (error) {
+    console.error("Error fetching coin overview", error);
+    return <CoinOverviewFallback />;
+  }
 
-        return (
-            <div id={"coin-overview"}>
-                <CandleSticksChart data={coinOHLCData} coinId={"bitcoin"} liveInterval={"1s"}
-                                   setLiveInterval={function (interval: "1s" | "1m"): void {
-                                       throw new Error("Function not implemented.");
-                                   }}>
-                    <div className={"header pt-2"}>
-                        <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-                        <div className={"info"}>
-                            <p>{coin.name} / {coin.symbol.toUpperCase()}</p>
-                            <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
-                        </div>
-                    </div>
-                </CandleSticksChart>
-            </div>
-        )
+  const [coin, coinOHLCData] = data;
 
-    } catch (error){
-       console.error("Error fetching coin overview", error)
-        return <CoinOverviewFallback />
-    }
-}
-export default CoinOverview
+  return (
+    <div id={"coin-overview"}>
+      <CandleSticksChart data={coinOHLCData} coinId={"bitcoin"}>
+        <div className={"header pt-2"}>
+          <Image
+            src={coin.image.large}
+            alt={coin.name}
+            width={56}
+            height={56}
+          />
+          <div className={"info"}>
+            <p>
+              {coin.name} / {coin.symbol.toUpperCase()}
+            </p>
+            <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+          </div>
+        </div>
+      </CandleSticksChart>
+    </div>
+  );
+};
+export default CoinOverview;
