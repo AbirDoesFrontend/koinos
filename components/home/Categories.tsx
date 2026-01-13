b0d2473a6005beea3982536502c1f03765bb6689
abirdoesfrontend <abirkhan.official.tsx@gmail.com>
2026-01-13 22:52:37 +0600

    add: categories

---
+++ components/home/Categories.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {fetcher} from "@/lib/coingecko.actions";
+import DataTable from "@/components/DataTable";
+
+const Categories = async() => {
+    const categories = await fetcher<Category[]>('/coins/categories');
+
+    const columns:DataTableColumn<Category>[] = [
+        {
+            header: "Category",
+            cellClassName: 'category-cell',
+            cell: (category) => category.name
+        }
+    ]
+
+    return (
+        <div id={"categories"} className={"custom-scrollbar"}>
+            <h4>Top Categories</h4>
+
+            <DataTable columns={columns} data={categories.slice(0, 10)} rowKey={(_, index) => index} tableClassName={"mt-3"}/>
+        </div>
+    )
+}
+export default Categories
